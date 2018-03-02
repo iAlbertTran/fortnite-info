fd14f4c79ae36e7344d63bb4c7fe3861e7ac34ea
Albert Tran <albtran@ucdavis.edu>
2018-03-02 04:35:02 -0800

    added user forms to search users

--- src/index.js
+++ src/index.js
@@ -2,6 +2,136 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import $ from "jquery";
+
+class Form extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			users: ['', ''],
+			platforms: ['pc', 'pc'],
+			forms: [true, true]
+		};
+
+		this.handleUserChange = this.handleUserChange.bind(this);
+		this.handlePlatformChange = this.handlePlatformChange.bind(this);
+		this.requestData = this.requestData.bind(this);
+		this.addForm = this.addForm.bind(this);
+	}
+
+
+	//updates username info
+	handleUserChange(event){
+		let index = parseInt(event.target.className[0], 10);
+		let users = this.state.users;
+		users[index] = event.target.value;
+
+		this.setState({users: users});
+	}
+
+
+	//updates platform info
+	handlePlatformChange(event){
+		let index = parseInt(event.target.className[0], 10);
+		let platforms = this.state.platforms;
+		platforms[index] = event.target.value;
+
+		this.setState({platforms: platforms});
+	}
+
+	//requests data from api
+	requestData(event){
+		//prevents page reload
+		event.preventDefault();
+
+		let users = this.state.users;
+		let platforms = this.state.platforms;
+		let user;
+		let platform;
+		let data = [];
+
+		let wait = 0;
+		//proxyurl needed to bypass cors
+		const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+		//for each user submitted
+		for(let i = 0; i < users.length; ++i){
+			user = users[i];
+			platform = platforms[i];
+			const url = 'https://api.fortnitetracker.com/v1/profile/' + platform.toLowerCase() + "/" + user;
+			const req = new XMLHttpRequest();
+			req.onreadystatechange = function(){
+				let newData;
+				if(req.readyState === XMLHttpRequest.DONE){
+					newData = JSON.parse(req.responseText);
+
+					if(newData.error){
+						newData.error = users[i];
+					}
+
+					data.push(newData);
+					this.props.handleNewInfo(data);
+				}
+			}.bind(this)
+
+			req.open('GET', proxyurl + url, true);
+
+			//api key send with request through header
+			req.setRequestHeader('TRN-api-key', '40e833dc-c903-4409-a3c0-d3f6d4cc0fa7');
+
+			//api limits 1 request per 2 seconds
+			setTimeout(function(){
+				req.send();
+			}, wait);
+
+			wait += 2000;
+		}
+
+	}
+	//adds additional forms for more users
+	addForm(event){
+		event.preventDefault();
+		const formNumber = this.state.forms;
+		const users = this.state.users;
+		const platforms = this.state.platforms;
+		this.setState({
+			users: users.concat(['']),
+			platforms: platforms.concat(['pc']),
+			forms: formNumber.concat([true])
+		});
+
+	}
+
+	render(){
+		let formNumber = this.state.forms;
+		const forms = formNumber.map((curr, index) => {
+			return(
+				<div key={index} className="userForm">
+					Name:
+					<input className={index} type='text' value={this.state.users[index]} onChange={this.handleUserChange} />
+					Platform:
+					<select className={index} value={this.state.platforms[index]} onChange={this.handlePlatformChange}>
+						<option>PC</option>
+						<option>PSN</option>
+						<option>XBL</option>
+					</select>
+				</div>
+			);
+
+		});
+
+		return(
+			<form id="playerForm" onSubmit={this.requestData}>
+				<label>
+					{forms}
+				</label>
+				<input type='submit' value='Get Info' />
+				<button onClick={this.addForm}>Add Player</button>
+			</form>
+		);
+	}
+
+}
+
 class Site extends React.Component{
 	constructor(props){
 		super(props);
@@ -10,45 +140,32 @@ class Site extends React.Component{
 		}
 	}
 
-	requestData(){
-
-		//proxyurl needed to bypass cors
-		const proxyurl = "https://cors-anywhere.herokuapp.com/";
-		const url = 'https://api.fortnitetracker.com/v1/profile/psn/davesteezy422';
-		let req = new XMLHttpRequest();
-		req.onreadystatechange = function(){
-			let data = this.state.data;
-			let newData;
-			if(req.readyState === XMLHttpRequest.DONE){
-				//console.log(req.responseText);
-				newData = JSON.parse(req.responseText);
-				this.setState({
-					data: data.concat(newData)
-				});
-				console.log(this.state.data);
-				return data;
-			}
-		}.bind(this);
-
-		req.open('GET', proxyurl + url, true);
-
-		//api key send with request through header
-		req.setRequestHeader('TRN-api-key', '40e833dc-c903-4409-a3c0-d3f6d4cc0fa7');
-
-		req.send();
+	//completely rewrite data with the new set of data
+	handleNewInfo(newData){
+		this.setState({
+			data: newData
+		});
 	}
 
 	render(){
-		const check = this.state.data;
-		let list = [];
-		for(let i = 0; i < check.length; ++i){
-			list.push(check[i].accountId);
-		}
-		
+
+		const data = this.state.data;
+		//parses the data to get necessary info
+		const parseUserData = data.map((curr, index) => {
+
+			const user = (!curr.error) ? 
+				curr.epicUserHandle : 
+				"Player named " + curr.error + " not found. Please Try Again."
+				
+				return(
+					<div key={user} className="userID" id={user}>{user}</div>
+				);
+		});  
+
 		return(
 			<div>
-				<button onClick={() => this.requestData()}>Yes</button>
-				{list}
+				<Form data={this.state.data} handleNewInfo={(newData) => this.handleNewInfo(newData)}/>
+				{parseUserData}
 			</div>
 		);
 	}
